b590364a0da8cdfb17e443170fa77b861c63db54
Jon Gregorowicz <jonathan.gregorowicz@gmail.com>
2016-10-13 16:26:46 -0400

    fixing

--- artopcommenters.py
+++ artopcommenters.py
@@ -1,40 +1,43 @@
 import praw
 import sys
 import pickle
+import operator
 
 AGENT='windows:blood_bender.reddit-data:v1.0.1 (by /u/blood_bender)'
 reddit = praw.Reddit(user_agent=AGENT)
 
 
 def main():
+  print("Warning: this takes a tooonnnnn of time, sorry")
   if (len(sys.argv) != 2):
     print("Must pass in postid")
     sys.exit(1)
 
   postid = sys.argv[1]
 
-  ret = query(postid)
-  # ret = read(postid)
-
-
+  if postid[-4:] == '.dat':
+    ret = read(postid)
+  else:
+    ret = query(postid)
 
   users = ret['users']
 
   topusers = sorted(users.items(), key=operator.itemgetter(1), reverse=True)
 
-  pusers = topusers[0:10]
+  pusers = topusers[0:7]
 
   print('* {0}'.format(ret['title']))
-  print('  * ' + ', '.join(map(lambda x: '({0}) /u/{1}'.format(x[0], x[1]))))
+  print('  * ' + ', '.join(map(lambda x: '({0}) /u/{1}'.format(x[1], x[0]), pusers)))
 
 
 def read(postid):
-  with open('{0}.dat'.format(postid), 'r') as dumpfile:
+  with open(postid, 'r') as dumpfile:
     ret = pickle.load(dumpfile)
     return ret
 
 def query(postid):
   post = reddit.get_submission(submission_id=postid, comment_limit=None)
+  # here's your bottleneck :(
   post.replace_more_comments(limit=None, threshold=0)
   comments = praw.helpers.flatten_tree(post.comments)
 
